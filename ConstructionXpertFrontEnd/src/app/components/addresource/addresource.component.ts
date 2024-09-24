f3c48dd2ee0f654c8c55dc80b7cb42ec23cd77f6
hajarhjr <hamimhajar85@gmail.com>
2024-09-24 09:35:31 +0100

    some changes in the code

--- ConstructionXpertFrontEnd/src/app/components/addresource/addresource.component.ts
+++ ConstructionXpertFrontEnd/src/app/components/addresource/addresource.component.ts
@@ -8,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './addresource.component.css'
 })
 export class AddresourceComponent {
+  resourceForm: FormGroup;
+  resourceTypes = Object.values(ResourceType);
 
+  constructor(private resourceService: ResourceService, private fb: FormBuilder) {
+    this.resourceForm = this.fb.group({
+      name: ['', Validators.required],
+      quantity: [1, Validators.required],
+      type: ['', Validators.required],
+      provider: ['', Validators.required],
+      taskId: [null, Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.resourceForm.valid) {
+      const resource: Resource = this.resourceForm.value;
+      this.resourceService.addResource(resource).subscribe({
+        next: (createdResource) => {
+          console.log('Resource created:', createdResource);
+          this.resourceForm.reset();
+        },
+        error: (error) => {
+          console.error('Error creating resource:', error);
+        },
+      });
+    }
+  }
+}
 }
